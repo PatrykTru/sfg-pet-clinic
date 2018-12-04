7258ceaf4e95e1477f2cf536f7dd0cd0b2616e98
Patryk Truchan <patryk.truchan94@gmail.com>
2018-12-04 17:44:21 +0100

    Added Specialties Map Impl.Closes #31.

---
+++ pet-clinic-data/src/main/java/tru/springframework/com/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package tru.springframework.com.sfgpetclinic.services;
+
+import tru.springframework.com.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
