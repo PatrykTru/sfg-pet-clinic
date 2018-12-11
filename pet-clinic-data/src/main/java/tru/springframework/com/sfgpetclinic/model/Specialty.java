7fa51a17f5348f2161a2b41ef4241221a6286e10
Patryk Truchan <patryk.truchan94@gmail.com>
2018-12-11 17:22:48 +0100

    Updated Files with JPA annotions. Closes #37

--- pet-clinic-data/src/main/java/tru/springframework/com/sfgpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/tru/springframework/com/sfgpetclinic/model/Specialty.java
@@ -1,6 +1,14 @@
 package tru.springframework.com.sfgpetclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity {
+
+    @JoinColumn(name = "description")
     private String description;
 
     public String getDescription() {
