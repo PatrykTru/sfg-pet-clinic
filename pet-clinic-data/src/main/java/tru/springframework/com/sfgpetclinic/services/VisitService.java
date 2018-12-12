04ef0e4564dc6327fe4a3dcaaee5703604c26f0d
Patryk Truchan <patryk.truchan94@gmail.com>
2018-12-12 17:31:55 +0100

    update

---
+++ pet-clinic-data/src/main/java/tru/springframework/com/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,7 @@
+package tru.springframework.com.sfgpetclinic.services;
+
+
+import tru.springframework.com.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+}
