8e6c3258af6bb0453adac34dfab59e87215787f7
Patryk Truchan <patryk.truchan94@gmail.com>
2018-12-13 19:55:11 +0100

    Added Project Lombok and Refactored files. Closes #51

--- pet-clinic-data/src/main/java/tru/springframework/com/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/tru/springframework/com/sfgpetclinic/model/BaseEntity.java
@@ -1,11 +1,19 @@
 package tru.springframework.com.sfgpetclinic.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @MappedSuperclass
 public class BaseEntity implements Serializable {
 
