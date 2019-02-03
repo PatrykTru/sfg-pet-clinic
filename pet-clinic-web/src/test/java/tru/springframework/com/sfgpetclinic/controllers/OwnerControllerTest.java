c27e1423cd6e28f02ec163a8a410611f7d2cd514
Patryk Truchan <patryk.truchan94@gmail.com>
2019-02-03 19:50:29 +0100

    display Owner Record and fixes bug. Closes #61.Closes #53

--- pet-clinic-web/src/test/java/tru/springframework/com/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/tru/springframework/com/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,23 +1,28 @@
 package tru.springframework.com.sfgpetclinic.controllers;
 
+
+import tru.springframework.com.sfgpetclinic.model.Owner;
+import tru.springframework.com.sfgpetclinic.services.OwnerService;
+
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import tru.springframework.com.sfgpetclinic.model.Owner;
-import tru.springframework.com.sfgpetclinic.services.OwnerService;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,8 +39,8 @@ class OwnerControllerTest {
     @BeforeEach
     void setUp() {
         owners = new HashSet<>();
-        owners.add(Owner.builder().id(1L).build());
-        owners.add(Owner.builder().id(2L).build());
+        owners.add(Owner.builder().id(1l).build());
+        owners.add(Owner.builder().id(2l).build());
 
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
@@ -65,5 +70,15 @@ class OwnerControllerTest {
         .andExpect(view().name("notImplemented"));
 
         verifyZeroInteractions(ownerService);
+    }
+    @Test
+    void displayOwner() throws Exception {
+
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
     }
 }
